e7c6f189f880962ef95a629727f2dcd56ba45238
Leonardo Besen <leo_besen@yahoo.com.br>
2020-05-06 20:46:24 -0300

    Setei a interface comparable na classe mae Curso

--- UniversidadeAmazonia/src/main/java/com/mycompany/universidadeamazonia/Curso.java
+++ UniversidadeAmazonia/src/main/java/com/mycompany/universidadeamazonia/Curso.java
@@ -1,6 +1,6 @@
 package com.mycompany.universidadeamazonia;
 
-public abstract class Curso {
+public abstract class Curso implements Comparable<Curso>{
     protected String nome;
     protected int anoCurso;
     protected String nivel;
@@ -33,4 +33,54 @@ public abstract class Curso {
     public String nomeDoArquivo(){
         return "/" + this.getNome() + "_" + this.getNivel() + "_" + this.getAnoCurso() + ".csv";
     }
+
+    @Override
+    public int compareTo(Curso outro){
+        if(!(getAnoCurso() == outro.getAnoCurso())) {
+            return getNome().compareTo(outro.getNome());
+	}
+        
+        if(!getNivel().equals(outro.getNivel())) {
+            return getNivel().compareTo(outro.getNivel());
+	}
+        
+        if(!getNome().equals(outro.getNome())) {
+            return getNome().compareTo(outro.getNome());
+	}
+        
+        return 0;
+    }
+    
+    @Override
+    public boolean equals(Object outro) {
+       	
+	if(this == outro) {
+            return true;
+        }
+        
+        if(outro == null) {
+            return false;
+        }
+        
+        if( ! (outro instanceof Curso) ){
+            return false;
+        }
+        
+	Curso outroCurso = (Curso)outro;
+        
+        if(! (this.getAnoCurso() == outroCurso.getAnoCurso()) ){
+            return false;
+        }
+        
+	if(! this.getNome().equals(outroCurso.getNome())){
+            return false;
+        }
+        
+        if(! this.getNivel().equals(outroCurso.getNivel())){
+            return false;
+        }
+        
+	return true;
+    }  
+    
 }
--- UniversidadeAmazonia/src/main/java/com/mycompany/universidadeamazonia/CursoGrad.java
+++ UniversidadeAmazonia/src/main/java/com/mycompany/universidadeamazonia/CursoGrad.java
@@ -1,60 +1,10 @@
 package com.mycompany.universidadeamazonia;
 
-public class CursoGrad extends Curso implements Comparable<CursoGrad>{
+public class CursoGrad extends Curso{
     
     public CursoGrad(String nome, int anoCurso){
         this.nivel = "GRADUACAO";
         this.nome = nome.toUpperCase();
         this.setAnoCurso(anoCurso);
-    }
-
-    @Override
-    public int compareTo(CursoGrad outro) {
-        
-        if(!(getAnoCurso() == outro.getAnoCurso())) {
-            return getNome().compareTo(outro.getNome());
-	}
-        
-        if(!getNivel().equals(outro.getNivel())) {
-            return getNivel().compareTo(outro.getNivel());
-	}
-        
-        if(!getNome().equals(outro.getNome())) {
-            return getNome().compareTo(outro.getNome());
-	}
-        	
-	return 0;
-    }
-    
-    @Override
-    public boolean equals(Object outro) {
-		
-	if(this == outro) {
-            return true;
-        }
-        
-        if(outro == null) {
-            return false;
-        }
-        
-        if( ! (outro instanceof CursoGrad) ){
-            return false;
-        }
-        
-	CursoGrad outroCurso = (CursoGrad)outro;
-        
-        if(! (this.getAnoCurso() == outroCurso.getAnoCurso()) ){
-            return false;
-        }
-        
-	if(! this.getNome().equals(outroCurso.getNome())){
-            return false;
-        }
-        
-        if(! this.getNivel().equals(outroCurso.getNivel())){
-            return false;
-        }
-        
-	return true;
     }
 }
